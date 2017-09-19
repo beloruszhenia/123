c4b6747be6918a20d1290af52a9cfb4ada41b3ab
beloruszhenia <beloruszhenia@gmail.com>
2017-09-19 13:34:15 +0300

    111

--- test.cmd
+++ test.cmd
@@ -5,7 +5,7 @@ net stop msiserver
 ren C:\Windows\SoftwareDistribution SoftwareDistribution.old
 ren C:\Windows\System32\catroot2 Catroot2.old
 123
-
+123444
 net start wuauserv
 net start cryptSvc
 net start bits
